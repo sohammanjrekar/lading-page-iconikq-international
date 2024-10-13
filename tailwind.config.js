039ce3bab95c510947284b2cbb776918fac689c2
soham shrikant manjrekar <mrsohammanjrekar@gmail.com>
2024-10-14 01:42:58 +0530

    upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './**/*.html',  // All HTML files
+    './src/**/*.{js,ts,jsx,tsx}', // All JS/TS and JSX/TSX files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
