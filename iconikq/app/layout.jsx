feea7864f4cfb10313ad25667acc67cd8efc47c0
soham shrikant manjrekar <mrsohammanjrekar@gmail.com>
2024-10-25 22:58:26 +0530

    Update layout.jsx

--- iconikq/app/layout.jsx
+++ iconikq/app/layout.jsx
@@ -4,6 +4,7 @@ import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
 import Footer from './layout/Footer';
 import Navbar from './layout/Navbar';
+import Head from 'next/head';
 
 // Import the Raleway font from the local file
 const raleway = localFont({
@@ -12,12 +13,10 @@ const raleway = localFont({
   display: 'swap',
 });
 
-
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={raleway.variable}>
-      <head>
+      <Head>
         {/* Favicon and Manifest setup */}
         <link rel="icon" href="/images/favicon.ico" sizes="any" type="image/x-icon" />
         <link rel="apple-touch-icon" href="/images/apple-touch-icon.png" sizes="180x180" />
@@ -26,7 +25,15 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/images/favicon-32x32.png" sizes="32x32" type="image/png" />
         <link rel="icon" href="/images/favicon-16x16.png" sizes="16x16" type="image/png" />
         <link rel="manifest" href="/site.webmanifest" />
-      </head>
+        <meta name="author" content="Iconikq International" />
+
+        {/* Open Graph Metadata */}
+        <meta property="og:title" content="Iconikq International - Logistics Solutions" />
+        <meta property="og:description" content="Discover efficient and reliable logistics services tailored for your business needs." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.iconikq.com" />
+        <meta property="og:image" content="/images/og-image.png" />
+      </Head>
       <body className="antialiased">
         <Navbar />
         {children}
