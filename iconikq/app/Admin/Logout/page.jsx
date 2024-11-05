fa0821e3eb9c3a3e116ca97218c9112c72a3d508
soham shrikant manjrekar <mrsohammanjrekar@gmail.com>
2024-11-05 13:52:31 +0530

    uplo

--- iconikq/app/Admin/Logout/page.jsx
+++ iconikq/app/Admin/Logout/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import { useSessionStore } from '../stores/sessionStore';
 import { useRouter } from 'next/navigation';
 
--- iconikq/app/Services/[slug]/[id]/page.jsx
+++ iconikq/app/Services/[slug]/[id]/page.jsx
@@ -1,4 +1,4 @@
-import { data } from '../../../Data';
+
 import Link from "next/link";
 
 
