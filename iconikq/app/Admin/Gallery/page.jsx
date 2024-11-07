c56f3822be46056ebf1c9c59edb292b124c4d13e
soham shrikant manjrekar <mrsohammanjrekar@gmail.com>
2024-11-07 16:50:57 +0530

    upload

--- iconikq/app/Admin/Gallery/page.jsx
+++ iconikq/app/Admin/Gallery/page.jsx
@@ -204,6 +204,8 @@ const GalleryPage = () => {
         <h3 className="text-2xl text-center font-semibold text-gray-800 mb-4">Gallery Items</h3>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {galleryItems.map(item => (
+            
+              item && 
             <div key={item.id} className="gallery-item bg-white p-4 shadow-md">
               <h4 className="text-lg font-semibold">Title: {item.title}</h4>
               <p className="text-sm text-gray-700 my-2">Description: {item.description}</p>
--- iconikq/app/Admin/page.jsx
+++ iconikq/app/Admin/page.jsx
@@ -87,7 +87,7 @@ const BlogAdmin = () => {
         </div>
         <div className="mt-12">
 
-          <div className="mb-12 grid gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-4">
+          <div className="mb-12 grid gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-3">
             {/* Blog Count Card */}
 
             <Link href={'/Admin/Blog'}>
