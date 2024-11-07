8b78af4634fa9307ce7316c22bf5e07c2627e9d4
soham shrikant manjrekar <mrsohammanjrekar@gmail.com>
2024-11-07 19:52:14 +0530

    Update layout.jsx

--- iconikq/app/layout.jsx
+++ iconikq/app/layout.jsx
@@ -6,11 +6,10 @@ import Footer from './layout/Footer';
 import Navbar from './layout/Navbar';
 import Head from 'next/head';
 import { SpeedInsights } from '@vercel/speed-insights/next';
- 
 
 // Import the Raleway font from the local file
 const raleway = localFont({
-  src: '../public/Raleway.ttf', // Path to the font file
+  src: '/Raleway.ttf', // Path to the font file
   variable: '--font-raleway', // CSS variable to reference this font
   display: 'swap',
 });
@@ -28,19 +27,20 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/images/favicon-16x16.png" sizes="16x16" type="image/png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="author" content="Iconikq International" />
-
-        <meta property="og:title" content="Iconikq International - Logistics Solutions"/>
-<meta property="og:description" content="Discover efficient and reliable logistics services tailored for your business needs."/>
-<meta property="og:image" content="https://64.media.tumblr.com/e724b44dfdb02b39cfd60979183fcab4/fcc8680e411f77fc-3c/s1280x1920/c5efbaa434e0112d1c12651ca5596b7fa2da83af.pnj"/>
-<meta property="og:url" content="https://www.iconikqinternational.com/"/>
-<meta property="og:type" content="website"/>
+        
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content="Iconikq International - Logistics Solutions" />
+        <meta property="og:description" content="Discover efficient and reliable logistics services tailored for your business needs." />
+        <meta property="og:image" content="https://64.media.tumblr.com/e724b44dfdb02b39cfd60979183fcab4/fcc8680e411f77fc-3c/s1280x1920/c5efbaa434e0112d1c12651ca5596b7fa2da83af.pnj" />
+        <meta property="og:url" content="https://www.iconikqinternational.com/" />
+        <meta property="og:type" content="website" />
       </Head>
       <body className="antialiased">
         <Navbar />
         {children}
+        <Footer />
         <Analytics />
         <SpeedInsights />
-        <Footer />
       </body>
     </html>
   );
